bbb93025be4ae5e24c9c3706425eddefde8d5f2e
now621 <now621@gmail.com>
2019-05-10 21:49:07 +0900

    #2019.05.10

--- ch1.4/condition.py
+++ ch1.4/condition.py
@@ -13,3 +13,40 @@ if a >5:
     print('Big')
 else:
     print('small')
+
+n = -2
+if n>0:
+    print('양수')
+elif n<0:
+    print('음수')
+else:
+    print('0')
+
+
+#
+#  spam : 100
+#  egg : 500
+#  spagetti = 2000
+
+price = 0
+goods = 'spam'
+if goods == 'spam':
+    price = 100
+elif goods == 'egg':
+    price = 500
+elif goods == 'spagetti':
+    price = 2000
+else:
+    price = 0
+print(price)
+
+
+#삼항 연산자
+#다른 언어:
+#
+# message = a > 5 ? 'big' : 'small'
+# print(message)
+
+#파이썬:
+a = 10
+print( 'big' if a>5 else 'small')
---
+++ ch1.4/while.py
@@ -0,0 +1,11 @@
+#while
+count = 1
+while count <5:
+    print(count)
+    count +=1
+
+print([c for c in range(5)])
+
+for c in range(11):
+    c += c
+print(c)
